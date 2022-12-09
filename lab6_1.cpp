05e197ea5ed2161f59e7c00783d27bd9d3518e62
monophoto <jetsadawut1013@gmail.com>
2022-12-09 16:28:03 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,30 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int integer[100] = {};
+    int numbers = 1;
+    int Evennumbers = 0;
+    int Oddnumbers = 0;
+    int i = 0;
+    while(numbers != 0){
+        cout << "Enter an integer: ";
+        cin >> numbers;
+        integer[i] = numbers;
+        i++;
+    }
+
+    int j = 0;
+    while(j < (i-1)){
+        if(integer[j]%2 == 0){
+            Evennumbers++;
+            j++;
+
+        }else{
+            Oddnumbers++;
+            j++;
+        }
+    }
+    cout << "#Even numbers = " << Evennumbers << "\n";
+    cout << "#Odd numbers = " << Oddnumbers;
     return 0;
 }
